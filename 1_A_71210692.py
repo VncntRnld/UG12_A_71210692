8f5e6132cc1a5962f4dfdc22925400e822435881
VincentR <vincent.ronald@ti.ukdw.ac.id>
2021-12-02 17:17:51 +0700

    Nambah yang ga ada yang sama, sama bilangan pertama

--- 1_A_71210692.py
+++ 1_A_71210692.py
@@ -1,17 +1,41 @@
 lst = list(input("Masukkan deret angka : ").split(","))
 
 ngt = []
+jgn = []
+pertama = 0
 
 print("Total: ", end="")
 for i in lst:
+    while (pertama < 1):
+        jgn.append(int(i))
+
+        if int(i) % 2 != 0:
+            Mns = int(i) * (-1)
+            ngt.append(Mns)
+            print(" - ", i, end="")
+            
+
+        if int(i) % 2 == 0:
+            pls = int(i)      
+            ngt.append(pls)           
+            print(" + ",pls, end="")
+            
+        pertama = pertama + 1
+
+#==================================================>
+
+    if int(i) in jgn:
+        continue
+
     if int(i) % 2 != 0:
         Mns = int(i) * (-1)
         ngt.append(Mns)
         print(" - ", i, end="")
+            
 
     if int(i) % 2 == 0:
         pls = int(i)      
-        ngt.append(pls)
+        ngt.append(pls)           
         print(" + ",pls, end="")
 
 print("\nHasil Perhitungan diatas ialah ", sum(ngt))
